72b86192bd6fc2151323f3029784943ab003f060
Do Duc Duong <duongdd2@vingroup.net>
2023-02-08 15:11:00 +0700

    Countdown

--- Countdown/Main.js
+++ Countdown/Main.js
@@ -1,11 +1,18 @@
 async function countDown() {
-    let timer = document.getElementById("timer").value;
+	let timeElm = document.getElementById("timer");
+    let timer = timeElm.textContent;
     let timeArr = timer.split(':');
-    let timeInSeconds = timeArr[0] * 3600 + timeArr[1] * 60 + timeArr[2];
-    await new Promise((resolve, reject) => {
-        setTimeout(() => {
-            console.log(timeInSeconds);
-            timeInSeconds -= 1;
-    }, timeInSeconds * 1000)});
-    console.log("aaaaaaaaa");
+    let timeInSeconds = timeArr[0] * 60 + Number(timeArr[1]);
+    var interval = setInterval(() => {
+		timeInSeconds -= 1;
+		let minutes = Math.floor(timeInSeconds / 60) + '';
+		minutes = minutes.length == 1 ? '0' + minutes : minutes;
+		let seconds = timeInSeconds % 60 + '';
+		seconds = seconds.length == 1 ? '0' + seconds : seconds;
+		timeElm.innerHTML = minutes + ':' + seconds;
+		
+		if (timeInSeconds == 0) {
+			clearInterval(interval);
+		}
+    }, 1000);
 }
